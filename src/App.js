e13c37d26aff0d2936d96fabdc9cdafe7c2a2a2e
aitzeng <aitzeng@gmail.com>
2024-08-03 18:49:01 -0700

    Create router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 /*global chrome*/
 import './App.css';
-import Login from './components/Login.jsx';
+import Login from './pages/Login.jsx';
+import Layout from './pages/Layout.jsx';
+import MyInfo from './pages/MyInfo.jsx';
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
@@ -17,11 +20,19 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login getCurrentTabId={getCurrentTabId} />
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Login getCurrentTabId={getCurrentTabId}/>} />
+          <Route path="MyInfo" element={<MyInfo />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Login getCurrentTabId={getCurrentTabId} />
+    //   </header>
+    // </div>
   );
 }
 
