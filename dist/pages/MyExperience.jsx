0be17dfba9b6e7e13e0de0b885fb94855ade12db
aitzeng <aitzeng@gmail.com>
2024-08-05 02:28:43 -0700

    Fixed string to boolean value in job.current

--- dist/pages/MyExperience.jsx
+++ dist/pages/MyExperience.jsx
@@ -7,6 +7,19 @@ function MyExperience({ getCurrentTabId }) {
   const [totalJobs, setTotalJobs] = useState(1);
   const [jobs, setJobs] = useState([]);
 
+  const monthArray = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+  const yearArray = [
+    "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959",
+    "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969",
+    "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979",
+    "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989",
+    "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999",
+    "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009",
+    "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019",
+    "2020", "2021", "2022", "2023", "2024"
+  ]
+
+
   const totalJobsHandler = (e) => {
     setTotalJobs(e.target.value);
   }
@@ -22,7 +35,7 @@ function MyExperience({ getCurrentTabId }) {
         storedJobs = [...storedJobs];
 
         for (let i = storedJobs.length; i < 5; i++) {
-          storedJobs.push({ jobTitle: '', company: '', location: '', current: false, roleDescription: '', startDate: '', endDate: '' });
+          storedJobs.push({ jobTitle: '', company: '', location: '', current: false, roleDescription: '', startDateMonth: '', startDateYear: '', endDateMonth: '', endDateYear: '' });
         }
 
         chrome.storage.local.set({ jobs: storedJobs })
@@ -55,7 +68,11 @@ function MyExperience({ getCurrentTabId }) {
 
   const handleInputChange = (index, field, value) => {
     const updatedJobs = [...jobs];
-    updatedJobs[index][field] = value;
+    if (field === 'current') {
+      updatedJobs[index][field] = value === 'true';
+    } else {
+      updatedJobs[index][field] = value;
+    }
     setJobs(updatedJobs);
   };
 
@@ -102,25 +119,57 @@ function MyExperience({ getCurrentTabId }) {
           <select value={job.current} onChange={(e) => {
             handleInputChange(index, 'current', e.target.value)
           }}>
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
           </select>
-          <div>Start Date</div>
-          <input placeholder="MM/YYYY" type="text" value={job.startDate} onChange={(e) => {
-            handleInputChange(index, 'startDate', e.target.value)
-          }} />
+          <div className="work-experience-title">Start Date</div>
+          <div className="start-date-container">
+            <div className="date-container">
+              <div>Month</div>
+              <select value={job.startDateMonth} onChange={(e) => {
+                handleInputChange(index, 'startDateMonth', e.target.value)
+              }}>
+                {monthArray.map((month) => <option value={month}>{month}</option>)}
+              </select>
+            </div>
+            <div className="date-container">
+              <div>Year</div>
+              <select value={job.startDateYear} onChange={(e) => {
+                handleInputChange(index, 'startDateYear', e.target.value)
+              }}>
+                {yearArray.map((year) => <option value={year}>{year}</option>)}
+              </select>
+            </div>
+          </div>
           {job.current ? null : <div>
-            <div>End Date</div>
-            <input placeholder="MM/YYYY" type="text" value={job.endDate} onChange={(e) => {
-              handleInputChange(index, 'endDate', e.target.value)
-            }} />
+            <div className="work-experience-title">End Date</div>
+            <div className="end-date-container">
+            <div className="date-container">
+              <div>Month</div>
+              <select value={job.endDateMonth} onChange={(e) => {
+                handleInputChange(index, 'endDateMonth', e.target.value)
+              }}>
+                {monthArray.map((month) => <option value={month}>{month}</option>)}
+              </select>
+            </div>
+            <div className="date-container">
+              <div>Year</div>
+              <select value={job.endDateYear} onChange={(e) => {
+                handleInputChange(index, 'endDateYear', e.target.value)
+              }}>
+                {yearArray.map((year) => <option value={year}>{year}</option>)}
+              </select>
+            </div>
+          </div>
+
           </div>}
           <div>Role Description</div>
           <textarea rows="10" cols="33" value={job.roleDescription} onChange={(e) => {
             handleInputChange(index, 'roleDescription', e.target.value)
           }}></textarea>
-        </div>)}
-    </form>
+        </div >)
+      }
+    </form >
   )
 }
 
--- src/scripts/myExperienceScript.js
+++ src/scripts/myExperienceScript.js
@@ -1,5 +1,5 @@
 /*global chrome*/
-(function() {
+(function () {
 
   let mimicTyping = (string, element) => {
     element.click();
@@ -36,20 +36,43 @@
     }
   }
 
+  let populateWorkExperience = (count, array) => {
+    for (let i = 0; i < count; i++) {
+      document.querySelector(`[data-automation-id="workExperience-${i + 1}"] [data-automation-id="jobTitle"]`).value = array[i].jobTitle;
+      document.querySelector(`[data-automation-id="workExperience-${i + 1}"] [data-automation-id="company"]`).value = array[i].company;
+      document.querySelector(`[data-automation-id="workExperience-${i + 1}"] [data-automation-id="location"]`).value = array[i].location;
+      let startDateElement = document.querySelector(`[data-automation-id="workExperience-${i + 1}"] [data-automation-id="formField-startDate"] [data-automation-id="dateSectionMonth-input"]`)
+      startDateElement.value = array[i].startDate;
+      if (array[i].current) {
+        document.querySelector(`[data-automation-id="workExperience-${i + 1}"] [data-automation-id="currentlyWorkHere"]`).click();
+      } else {
+        document.querySelector(`[data-automation-id="workExperience-${i + 1}"] [data-automation-id="formField-endDate""] [data-automation-id="dateInputWrapper"]`).value = array[i].endDate;
+      }
+      document.querySelector(`[data-automation-id="workExperience-${i + 1}"] [data-automation-id="description"]`).value = array[i].roleDescription;
+    }
+  }
+
   let populateMyExperience = () => {
     chrome.storage.local.get("totalJobs")
-    .then((result) => {
-      let totalJobCount = result.totalJobs; // Number of jobs set in the extension
-      let webPageJobCount = document.querySelectorAll('[data-automation-id="formField-jobTitle"]').length; // Number of jobs present on the web page
-      let jobCountDifference = totalJobCount - webPageJobCount; // If positive, extension > web page
-      adjustWorkCount(jobCountDifference);
-      // console.log("Extension Count:", totalJobCount);
-      // console.log("Web page count:", webPageJobCount);
-    })
-    // chrome.storage.local.get("jobs")
-    // .then((result) => {
-    //   console.log(result.jobs);
-    // })
+      .then((result) => {
+        let totalJobCount = result.totalJobs; // Number of jobs set in the extension
+        let webPageJobCount = document.querySelectorAll('[data-automation-id="formField-jobTitle"]').length; // Number of jobs present on the web page
+        let jobCountDifference = totalJobCount - webPageJobCount; // If positive, extension > web page
+        adjustWorkCount(jobCountDifference);
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(totalJobCount);
+          }, 100)
+        })
+      })
+      .then((count) => {
+        console.log('This is count:', count);
+        chrome.storage.local.get("jobs")
+          .then((result) => {
+            populateWorkExperience(count, result.jobs);
+            // console.log("Array of jobs:", result.jobs);
+          })
+      })
   }
 
   populateMyExperience();
