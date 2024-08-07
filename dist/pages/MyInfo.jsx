bc054280bf7b4c6b17d0bb483018d946b0e1b430
aitzeng <aitzeng@gmail.com>
2024-08-07 04:03:51 -0700

    Populate information storage on application start

--- dist/pages/MyInfo.jsx
+++ dist/pages/MyInfo.jsx
@@ -3,7 +3,7 @@ import '../styles/MyInfo.css';
 import { useState, useEffect } from 'react';
 import { listOfCountries, countriesWithCodes } from '../data.js';
 
-function MyInfo({getCurrentTabId}) {
+function MyInfo({ getCurrentTabId }) {
 
   const [previousEmployed, setPreviousEmployed] = useState('');
   const [country, setCountry] = useState('');
@@ -18,20 +18,64 @@ function MyInfo({getCurrentTabId}) {
   const [phoneNumber, setPhoneNumber] = useState('');
 
   const populateExtension = () => {
-    chrome.storage.local.get(["prevEmployed", "country", "firstName", "lastName", "address", "city", "region", "postalCode", "phoneType", "countryCode", "phoneNumber"])
+    // Default values to set if keys do not exist in storage
+    const defaultValues = {
+      prevEmployed: false, // or whatever default you prefer
+      country: 'United States of America',
+      firstName: '',
+      lastName: '',
+      address: '',
+      city: '',
+      region: '',
+      postalCode: '',
+      phoneType: 'Mobile',
+      countryCode: 'United States of America (+1)',
+      phoneNumber: ''
+    };
+
+    chrome.storage.local.get([
+      "prevEmployed",
+      "country",
+      "firstName",
+      "lastName",
+      "address",
+      "city",
+      "region",
+      "postalCode",
+      "phoneType",
+      "countryCode",
+      "phoneNumber"
+    ])
       .then((result) => {
-        setPreviousEmployed(result.prevEmployed);
-        setCountry(result.country);
-        setFirstName(result.firstName);
-        setLastName(result.lastName);
-        setAddress(result.address);
-        setRegion(result.region);
-        setCity(result.city);
-        setPostalCode(result.postalCode);
-        setCountryCode(result.countryCode);
-        setPhoneNumber(result.phoneNumber);
+        setPreviousEmployed(result.prevEmployed ?? defaultValues.prevEmployed);
+        setCountry(result.country ?? defaultValues.country);
+        setFirstName(result.firstName ?? defaultValues.firstName);
+        setLastName(result.lastName ?? defaultValues.lastName);
+        setAddress(result.address ?? defaultValues.address);
+        setCity(result.city ?? defaultValues.city);
+        setRegion(result.region ?? defaultValues.region);
+        setPostalCode(result.postalCode ?? defaultValues.postalCode);
+        setPhoneType(result.phoneType ?? defaultValues.phoneType);
+        setCountryCode(result.countryCode ?? defaultValues.countryCode);
+        setPhoneNumber(result.phoneNumber ?? defaultValues.phoneNumber);
+
+        const valuesToStore = {};
+        for (const key in defaultValues) {
+          if (result[key] === undefined) {
+            valuesToStore[key] = defaultValues[key];
+          }
+        }
+
+        if (Object.keys(valuesToStore).length > 0) {
+          chrome.storage.local.set(valuesToStore).then(() => {
+            console.log('Default values set:', valuesToStore);
+          });
+        }
       })
-  }
+      .catch((error) => {
+        console.error('Error retrieving storage values:', error);
+      });
+  };
 
   const autofillSubmitter = () => {
     getCurrentTabId()
@@ -43,54 +87,65 @@ function MyInfo({getCurrentTabId}) {
 
   const prevEmployedHandler = (event) => {
     setPreviousEmployed(event.target.value);
-    chrome.storage.local.set({ 'prevEmployed': event.target.value });
   }
 
   const countryHandler = (event) => {
     setCountry(event.target.value);
-    chrome.storage.local.set({ 'country': event.target.value });
   }
 
   const firstNameHandler = (e) => {
     setFirstName(e.target.value);
-    chrome.storage.local.set({ 'firstName': e.target.value });
   }
 
   const lastNameHandler = (e) => {
     setLastName(e.target.value);
-    chrome.storage.local.set({ 'lastName': e.target.value });
   }
   const addressHandler = (e) => {
     setAddress(e.target.value);
-    chrome.storage.local.set({ 'address': e.target.value });
   }
   const cityHandler = (e) => {
     setCity(e.target.value);
-    chrome.storage.local.set({ 'city': e.target.value });
   }
   const regionHandler = (e) => {
     setRegion(e.target.value);
-    chrome.storage.local.set({ 'region': e.target.value });
   }
   const postalCodeHandler = (e) => {
     setPostalCode(e.target.value);
-    chrome.storage.local.set({ 'postalCode': e.target.value });
   }
 
   const phoneTypeHandler = (e) => {
     setPhoneType(e.target.value);
-    chrome.storage.local.set({ 'phoneType': e.target.value });
   }
 
   const countryCodeHandler = (e) => {
     setCountryCode(e.target.value);
-    chrome.storage.local.set({ 'countryCode': e.target.value });
   }
 
   const phoneNumberHandler = (e) => {
     setPhoneNumber(e.target.value);
-    chrome.storage.local.set({ 'phoneNumber': e.target.value });
   }
+
+  const saveInformationHandler = () => {
+    chrome.storage.local.set({
+      prevEmployed: previousEmployed,
+      country: country,
+      firstName: firstName,
+      lastName: lastName,
+      address: address,
+      city: city,
+      region: region,
+      postalCode: postalCode,
+      phoneType: phoneType,
+      countryCode: countryCode,
+      phoneNumber: phoneNumber
+    })
+      .then(() => {
+        console.log('All information saved successfully');
+      })
+      .catch((error) => {
+        console.error('Error saving information:', error);
+      });
+  };
 
   useEffect(() => {
     populateExtension()
@@ -98,6 +153,7 @@ function MyInfo({getCurrentTabId}) {
 
   return (
     <form onSubmit={autofillSubmitter}>
+      <button type="button" onClick={saveInformationHandler}>Save</button>
       <button type="submit">Auto-fill</button>
       <div>Previously employed at this company?</div>
       <select value={previousEmployed} onChange={prevEmployedHandler}>
